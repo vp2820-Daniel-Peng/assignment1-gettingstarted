1ba4ff4841279ee38c30f02f450ef1842854879d
vp2820-Daniel-Peng <vp2820@nyu.edu>
2026-05-25 14:52:09 -0400

    assignment getting started

---
+++ gettingStarted.py
@@ -0,0 +1,39 @@
+def welcome_assignment_answers(question):
+
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "d830a5c57096088f58cef0786ea566eceab2ef3a7e50cdb4c5c64551dafaee4a"
+
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 7
+
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+
+    else:
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+
+    return(answer)
+
+
+if __name__ == "__main__":
+
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
